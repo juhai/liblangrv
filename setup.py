b4492df70ea24f54f77ae120d3e7f48613df0e40
Juha Iso-Sipilä <juha.iso-sipila@microsoft.com>
2017-06-12 10:38:46 +0100

    version upgrade to 0.1.1

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, Extension
 from glob import glob
 
 module = Extension('langrv',
-                   sources = glob('src/*.cpp') + glob('src/py/*.cpp'),
-                   extra_compile_args = ['-std=c++11'],
-                   include_dirs=['src/',])
+                   sources=glob('src/*.cpp') + glob('src/py/*.cpp'),
+                   extra_compile_args=['-std=c++11'],
+                   include_dirs=['src/', ])
 
-setup(name = 'langrv',
-      version = '0.1.0',
-      description = 'A simple implementation of Random Indexing For Language Detection.',
-      ext_modules = [module])
+setup(name='langrv',
+      version='0.1.1',
+      description='A simple implementation of Random Indexing For Language Detection.',  # noqa
+      ext_modules=[module])
